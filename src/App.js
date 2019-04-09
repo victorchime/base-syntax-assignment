6019932d3485b23b307674750eb072195a2f53ea
Anayo <victor.chime@gmail.com>
2019-04-09 21:57:58 +0100

    added state

--- src/App.js
+++ src/App.js
@@ -4,12 +4,16 @@ import UserOutput from './User/UserOutput'
 import './App.css';
 
 class App extends Component {
+  state = {
+      username: "user"
+  }
+
   render() {
     return (
       <div className="App">
-        <UserOutput username="vchime">Hello This is the first paragragh</UserOutput>
-        <UserOutput username="rmark">Hello This is the second paragragh</UserOutput>
-        <UserOutput username="vsmith">Hello This is the third paragragh</UserOutput>
+        <UserOutput username={this.state.username}>Hello This is the first paragragh</UserOutput>
+        <UserOutput username={this.state.username}>Hello This is the second paragragh</UserOutput>
+        <UserOutput username={this.state.username}>Hello This is the third paragragh</UserOutput>
       </div>
     );
   }
